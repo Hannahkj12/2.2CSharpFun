71fe7b8f50aa8e792499b414177fd931fbeac888
Hannahkj12 <hannahkj2001@gmail.com>
2024-01-16 16:03:48 -0700

    Add project files.

---
+++ PrintStuff.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2CSharpFun
+{
+    internal class PrintStuff
+    {
+        //must be here so its scope is for everything in here
+        private string language;
+
+        //constructor
+        public PrintStuff ()
+        {
+            language = "EN";
+        }
+        public PrintStuff(string temp) 
+        {
+            language = temp;
+        }
+        public void PrintName(string n)
+        {
+            if (language == "EN")
+            {
+                System.Console.WriteLine("Hello, " + n + "!");
+            }
+            if (language == "PT")
+            {
+                System.Console.WriteLine("Oi, " + n + "!");
+            }
+        }
+        public void PrintName()
+        {
+            if (language == "EN")
+            {
+                System.Console.WriteLine("Hello, John or Jane Doe!");
+            }
+            if (language == "PT")
+            {
+                System.Console.WriteLine("Oi, John ou Jane Doe");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using _2._2CSharpFun;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        PrintStuff ps = new PrintStuff("PT");
+
+        string name = "";
+
+        System.Console.WriteLine("Please enter your name");
+        name = System.Console.ReadLine();
+
+        //To print John or jane doe use this code: ps.PrintName();
+        ps.PrintName(name);
+    }
+}
